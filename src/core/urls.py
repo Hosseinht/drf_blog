72f2837f77b38588ea4d2161c3a23f9ee5c6fa90
Ryan <sailantht@gmail.com>
2023-01-09 10:11:30 +0330

    Documentation with swagger & redoc

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,10 +3,30 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="A simple blog api",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@blog.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include('blog.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
 
 if settings.DEBUG:
